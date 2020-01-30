dd264318f00266dd0af994c4bf82284daa97ee38
Felix Dollack <felix.dollack@googlemail.com>
2020-01-30 23:01:14 +0900

    Adjust indentation

--- src/ofApp.h
+++ src/ofApp.h
@@ -4,17 +4,16 @@
 #include "udpReceiverThread.hpp"
 
 class ofApp : public ofBaseApp{
+public:
+    void setup();
+    void update();
+    void draw();
 
-	public:
-		void setup();
-		void update();
-		void draw();
+    void keyPressed(int key);
+    void keyReleased(int key);
+    void windowResized(int w, int h);
+    void gotMessage(ofMessage msg);
 
-		void keyPressed(int key);
-		void keyReleased(int key);
-		void windowResized(int w, int h);
-		void gotMessage(ofMessage msg);
-
-        UdpReceiverThread* udp_thread;
-        int state;
+    UdpReceiverThread* udp_thread;
+    int state;
 };
